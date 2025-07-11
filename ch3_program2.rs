3c008b706fadf3ea7fcc8e0f02bb57a2fbefb838
VasthaviSreya-Thumu <156116582+VasthaviSreya-Thumu@users.noreply.github.com>
2025-07-11 09:49:40 +0530

    Update ch3_program2.rs

--- ch3_program2.rs
+++ ch3_program2.rs
@@ -1,17 +1,5 @@
-use std::io;
-
 fn main() {
-    println!("Enter a number:");
-
-    let mut input = String::new();
-
-    // Read input from the user
-    io::stdin().read_line(&mut input).expect("Failed to read input");
-
-    // Convert the input string to an integer (i32)
-    let number: i32 = input.trim().parse().expect("Please enter a valid number");
-
-    // Check if the number is even or odd using modulus operator
+    let input = "42"; 
     if number % 2 == 0 {
         println!("The number {} is even.", number);
     } else {
